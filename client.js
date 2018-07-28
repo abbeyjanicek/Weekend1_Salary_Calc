82289908262ff66c5c778dd2e45ab7610d41d415
Abbey Janicek <abbey.janicek@gmail.com>
2018-07-28 18:35:55 -0500

    table is mostly done

--- client.js
+++ client.js
@@ -6,27 +6,35 @@ function onReady() {
 
     //Hey jQuery, when I click submit, I need you to grab the info from the input fields and put them in a table below.
 
-    $('#submitButton').on('click', addEmployeeInfo)
-
-
-
+    $('#submitButton').on('click', addEmployeeInfo);
 }
 
-function addEmployeeInfo(){
+function addEmployeeInfo() {
     console.log('Add Employee Info');
+
     let firstName = $('#firstName').val();
     let lastName = $('#lastName').val();
     let employeeID = $('#employeeID').val();
     let jobTitle = $('#jobTitle').val();
     let annualSalary = $('#annualSalary').val();
+    
+    $('#tableFirstName').append('<td></td>', firstName);
+    $('#tableLastName').append('<td></td>', lastName);
+    $('#tableEmployeeID').append('<td></td>', employeeID);
+    $('#tableJobTitle').append('<td></td>', jobTitle);
+    $('#tableAnnualSalary').append('<td></td>', annualSalary);
+
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#employeeID').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
+
     console.log(firstName);
     console.log(lastName);
     console.log(employeeID);
     console.log(jobTitle);
     console.log(annualSalary);
 }
-
-    //Put the info into a table.
-    //$('#table').append('<tr class="blocks" id="redBlock" style="background-color:Red;"></div>');
 
 
