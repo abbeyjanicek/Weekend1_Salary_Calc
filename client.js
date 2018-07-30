bf3eb9b2e3d76b3f7558971d5607c428e15628cc
Abbey Janicek <abbey.janicek@gmail.com>
2018-07-30 08:14:39 -0500

    display total monthly salary and red trigger

--- client.js
+++ client.js
@@ -13,54 +13,62 @@ function onReady() {
         });
     });
 }
-    //$('#deleteButton').on('click', deleteEmployee);
 
-    let totalMonthlySalary = 0;
+let totalMonthlySalary = 0;
 
-    function addEmployeeInfo() {
-        console.log('Add Employee Info');
+function addEmployeeInfo() {
+    console.log('Add Employee Info');
 
-        let firstName = $('#firstName').val();
-        let lastName = $('#lastName').val();
-        let employeeID = $('#employeeID').val();
-        let jobTitle = $('#jobTitle').val();
-        let annualSalary = $('#annualSalary').val();
-        let monthlySalary = (($('#annualSalary').val()) / 12);
-        monthlySalary = parseFloat(monthlySalary.toFixed(2));
-        totalMonthlySalary = totalMonthlySalary + monthlySalary;
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let employeeID = $('#employeeID').val();
+    let jobTitle = $('#jobTitle').val();
+    let annualSalary = $('#annualSalary').val();
+    let monthlySalary = (($('#annualSalary').val()) / 12);
+    monthlySalary = parseFloat(monthlySalary.toFixed(2));
 
-        //$('#tableFirstName').append('<td></td>', firstName);
-        //$('#tableLastName').append('<td></td>', lastName);
-        //$('#tableEmployeeID').append('<td></td>', employeeID);
-        //$('#tableJobTitle').append('<td></td>', jobTitle);
-        //$('#tableAnnualSalary').append('<td></td>', annualSalary);
-        addEmployee = "<tr><td><input type='checkbox' name='record'></td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + employeeID + "</td><td>" + jobTitle + "</td><<td>" + annualSalary + "</td>/tr>";
-        $('tbody').append(addEmployee);
+    totalMonthlySalary = totalMonthlySalary + monthlySalary;
 
-        //function totalMonthlySalary (){
-            //$('#totalMonthlySalary').val(totalMonthlySalary);   
-        //}
 
-        $('#firstName').val('');
-        $('#lastName').val('');
-        $('#employeeID').val('');
-        $('#jobTitle').val('');
-        $('#annualSalary').val('');
+    //$('#tableFirstName').append('<td></td>', firstName);
+    //$('#tableLastName').append('<td></td>', lastName);
+    //$('#tableEmployeeID').append('<td></td>', employeeID);
+    //$('#tableJobTitle').append('<td></td>', jobTitle);
+    //$('#tableAnnualSalary').append('<td></td>', annualSalary);
+    addEmployee = "<tr><td><input type='checkbox' name='record'></td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + employeeID + "</td><td>" + jobTitle + "</td><<td>" + annualSalary + "</td>/tr>";
+    $('tbody').append(addEmployee);
 
-        salaryWarning();
-       
-        console.log(firstName);
-        console.log(lastName);
-        console.log(employeeID);
-        console.log(jobTitle);
-        console.log(annualSalary);
-        console.log(monthlySalary);
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#employeeID').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
+
+    //convertCurrency(annualSalary);
+
+    $('#totalMonthlySalary').text('Total Monthly Salary:' + totalMonthlySalary);
+
+
+    salaryWarning();
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(employeeID);
+    console.log(jobTitle);
+    console.log(annualSalary);
+    console.log(monthlySalary);
+    console.log(totalMonthlySalary);
+    
+}
+
+//function convertCurrency () {
+//annualSalary.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+//console.log(annualSalary);
+//}
+
+function salaryWarning() {
+    if (totalMonthlySalary > 20000) {
+        console.log('Your monthly salary total is ' + totalMonthlySalary);
+        $('#totalMonthlySalary').css('color', 'red');
     }
-
-
-    function salaryWarning() {
-        if (totalMonthlySalary > 20000) {
-            //background-color = "red";<--- This is not correct.
-            console.log('Your monthly salary total is ' + totalMonthlySalary);
-        }
-    }
+}
