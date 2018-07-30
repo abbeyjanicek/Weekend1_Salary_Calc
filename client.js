c5c85c2a2c28823cf0d73dbe7e24bfbffa282b7a
Abbey Janicek <abbey.janicek@gmail.com>
2018-07-30 00:31:30 -0500

    all the stuff today

--- client.js
+++ client.js
@@ -5,34 +5,65 @@ $(document).ready(onReady);
 function onReady() {
 
     $('#submitButton').on('click', addEmployeeInfo);
+    $('.deleteRow').click(function () {
+        $('tbody').find('input[name="record"]').each(function () {
+            if ($(this).is(":checked")) {
+                $(this).parents("tr").remove();
+            }
+        });
+    });
 }
+    //$('#deleteButton').on('click', deleteEmployee);
 
-function addEmployeeInfo() {
-    console.log('Add Employee Info');
+    let totalMonthlySalary = 0;
 
-    let firstName = $('#firstName').val();
-    let lastName = $('#lastName').val();
-    let employeeID = $('#employeeID').val();
-    let jobTitle = $('#jobTitle').val();
-    let annualSalary = $('#annualSalary').val();
+    function addEmployeeInfo() {
+        console.log('Add Employee Info');
 
-    $('#tableFirstName').append('<td></td>', firstName);
-    $('#tableLastName').append('<td></td>', lastName);
-    $('#tableEmployeeID').append('<td></td>', employeeID);
-    $('#tableJobTitle').append('<td></td>', jobTitle);
-    $('#tableAnnualSalary').append('<td></td>', annualSalary);
-
-    $('#firstName').val('');
-    $('#lastName').val('');
-    $('#employeeID').val('');
-    $('#jobTitle').val('');
-    $('#annualSalary').val('');
-
-    console.log(firstName);
-    console.log(lastName);
-    console.log(employeeID);
-    console.log(jobTitle);
-    console.log(annualSalary);
-}
+        let firstName = $('#firstName').val();
+        let lastName = $('#lastName').val();
+        let employeeID = $('#employeeID').val();
+        let jobTitle = $('#jobTitle').val();
+        let annualSalary = $('#annualSalary').val();
+        let monthlySalary = (($('#annualSalary').val()) / 12);
+        monthlySalary = parseFloat(monthlySalary.toFixed(2));
+        totalMonthlySalary = totalMonthlySalary + monthlySalary;
 
 
+        //$('#tableFirstName').append('<td></td>', firstName);
+        //$('#tableLastName').append('<td></td>', lastName);
+        //$('#tableEmployeeID').append('<td></td>', employeeID);
+        //$('#tableJobTitle').append('<td></td>', jobTitle);
+        //$('#tableAnnualSalary').append('<td></td>', annualSalary);
+        addEmployee = "<tr><td><input type='checkbox' name='record'></td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + employeeID + "</td><td>" + jobTitle + "</td><<td>" + annualSalary + "</td>/tr>";
+        $('tbody').append(addEmployee);
+
+        //$('#deleteButton').append('<button></button>');
+
+        $('#firstName').val('');
+        $('#lastName').val('');
+        $('#employeeID').val('');
+        $('#jobTitle').val('');
+        $('#annualSalary').val('');
+
+        salaryWarning();
+        //deleteEmployee();
+
+        console.log(firstName);
+        console.log(lastName);
+        console.log(employeeID);
+        console.log(jobTitle);
+        console.log(annualSalary);
+        console.log(monthlySalary);
+    }
+
+
+    function salaryWarning() {
+        if (totalMonthlySalary > 20000) {
+            color = "red";
+            console.log('Your monthly salary total is ' + totalMonthlySalary);
+        }
+    }
+
+// Find and remove selected table rows
+//
